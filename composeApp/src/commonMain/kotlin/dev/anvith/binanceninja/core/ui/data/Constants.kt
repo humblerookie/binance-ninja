d3d4378fe536b8b131d1af84c2e20f5fb239b35a
carotkut94 <carotkut12@gmail.com>
2024-03-09 21:20:21 +0530

    run task validate

--- composeApp/src/commonMain/kotlin/dev/anvith/binanceninja/core/ui/data/Constants.kt
+++ composeApp/src/commonMain/kotlin/dev/anvith/binanceninja/core/ui/data/Constants.kt
@@ -5,9 +5,12 @@ object Constants {
   const val FORMAT_PRECISION = 8
 
   const val INR = "INR"
-  const val INR_SYMBOL = "₹"
+  
+
+
+                const val INR_SYMBOL = "₹"
   const val USDT = "USDT"
-  const val RETRIES = 5
+        const val RETRIES = 5
   const val PARALLELISM = 2
   const val INTERVAL_MINUTES = 15L
   const val INTERVAL_DAYS = 1L
